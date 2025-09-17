9b908f33164e4c2032028b91734490a0a5c0b6f2
MarieGouze <88887382+MarieGouze@users.noreply.github.com>
2025-09-17 14:49:57 +0800

    chore: 更新导航数据 - 2025/9/17 14:49:56

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -40,6 +40,13 @@ export const mockData = {
           "url": "https://y.wjhe.top/#/discover?platform=kuwo",
           "description": "",
           "icon": "https://marieniris.qzz.io/file/img/1756629622909_logo-icon.png"
+        },
+        {
+          "id": "site-1758091771963",
+          "name": "Moon Tv",
+          "url": "http://146.235.211.48:3001/",
+          "description": "",
+          "icon": "https://img.icons8.com/?size=100&id=qbXvnWotjMWC&format=png&color=000000"
         }
       ]
     },
