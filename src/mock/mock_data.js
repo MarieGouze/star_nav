d97d0c2486290eeee6aed2e05f33ab7685521d65
MarieGouze <88887382+MarieGouze@users.noreply.github.com>
2025-09-21 23:32:18 +0800

    chore: 更新导航数据 - 2025/9/21 23:32:16

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -429,7 +429,7 @@ export const mockData = {
         {
           "id": "site-1758466555129",
           "name": "StarPaste",
-          "url": "https://cloudpaste-frontend-89q.pages.dev/admin/dashboard",
+          "url": "https://cloudpaste-frontend-89q.pages.dev/",
           "description": "",
           "icon": "https://camo.githubusercontent.com/2d1d19cbb8580225db6fe3c93d285be9c2c8871e3d6469fdfc5a7151b377a5ae/68747470733a2f2f696d672e69636f6e73382e636f6d2f6475736b2f3130302f70617374652e706e67"
         }
